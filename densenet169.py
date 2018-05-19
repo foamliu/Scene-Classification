bd50bed2d1aac7ac9c0fcd16b8e64a3996afaeb1
Yang Liu <foamliu@yeah.net>
2018-05-19 10:11:32 +0800

    ImportError: No module named 'custom_layers'

--- densenet169.py
+++ densenet169.py
@@ -89,10 +89,10 @@ def densenet169_model(img_rows, img_cols, color_type=1, nb_dense_block=4, growth
 
     if K.image_dim_ordering() == 'th':
       # Use pre-trained weights for Theano backend
-      weights_path = 'imagenet_models/densenet169_weights_th.h5'
+      weights_path = 'models/densenet169_weights_th.h5'
     else:
       # Use pre-trained weights for Tensorflow backend
-      weights_path = 'imagenet_models/densenet169_weights_tf.h5'
+      weights_path = 'models/densenet169_weights_tf.h5'
 
     model.load_weights(weights_path, by_name=True)
 
