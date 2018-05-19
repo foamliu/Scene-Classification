be1decaca76972090c7003f258d330b878d0f828
Yang Liu <foamliu@yeah.net>
2018-05-19 10:37:41 +0800

    multi gpus to solve OOM

--- train.py
+++ train.py
@@ -2,8 +2,10 @@ import keras
 from keras.callbacks import ModelCheckpoint, EarlyStopping
 from keras.callbacks import ReduceLROnPlateau
 from keras.preprocessing.image import ImageDataGenerator
-
+import tensorflow as tf
 from densenet169 import densenet169_model
+from utils import get_available_gpus
+from keras.utils import multi_gpu_model
 
 img_width, img_height = 224, 224
 num_channels = 3
@@ -18,9 +20,6 @@ num_epochs = 100000
 patience = 50
 
 if __name__ == '__main__':
-    # build a classifier model
-    model = densenet169_model(img_rows=img_height, img_cols=img_width, color_type=num_channels, num_classes=num_classes)
-
     # prepare data augmentation configuration
     train_data_gen = ImageDataGenerator(rotation_range=20.,
                                         width_shift_range=0.1,
@@ -28,7 +27,36 @@ if __name__ == '__main__':
                                         zoom_range=0.2,
                                         horizontal_flip=True)
     valid_data_gen = ImageDataGenerator()
-    # callbacks
+
+    # generators
+    train_generator = train_data_gen.flow_from_directory(train_data, (img_width, img_height), batch_size=batch_size,
+                                                         class_mode='categorical')
+    valid_generator = valid_data_gen.flow_from_directory(valid_data, (img_width, img_height), batch_size=batch_size,
+                                                         class_mode='categorical')
+
+    class MyCbk(keras.callbacks.Callback):
+        def __init__(self, model):
+            keras.callbacks.Callback.__init__(self)
+            self.model_to_save = model
+
+        def on_epoch_end(self, epoch, logs=None):
+            fmt = 'models/' + 'model.%02d-%.4f.hdf5'
+            self.model_to_save.save(fmt % (epoch, logs['val_acc']))
+
+    # build a classifier model
+    num_gpu = len(get_available_gpus())
+    if num_gpu >= 2:
+        with tf.device("/cpu:0"):
+            model = densenet169_model(img_rows=img_height, img_cols=img_width, color_type=num_channels,
+                                      num_classes=num_classes)
+
+        new_model = multi_gpu_model(model, gpus=num_gpu)
+        # rewrite the callback: saving through the original model and not the multi-gpu model.
+        model_checkpoint = MyCbk(model)
+    else:
+        model = densenet169_model(img_rows=img_height, img_cols=img_width, color_type=num_channels, num_classes=num_classes)
+
+    # Callbacks
     tensor_board = keras.callbacks.TensorBoard(log_dir='./logs', histogram_freq=0, write_graph=True, write_images=True)
     early_stop = EarlyStopping('val_acc', patience=patience)
     reduce_lr = ReduceLROnPlateau('val_acc', factor=0.1, patience=int(patience / 4), verbose=1)
@@ -36,12 +64,6 @@ if __name__ == '__main__':
     model_names = trained_models_path + '.{epoch:02d}-{val_acc:.4f}.hdf5'
     model_checkpoint = ModelCheckpoint(model_names, monitor='val_acc', verbose=1, save_best_only=True)
     callbacks = [tensor_board, model_checkpoint, early_stop, reduce_lr]
-
-    # generators
-    train_generator = train_data_gen.flow_from_directory(train_data, (img_width, img_height), batch_size=batch_size,
-                                                         class_mode='categorical')
-    valid_generator = valid_data_gen.flow_from_directory(valid_data, (img_width, img_height), batch_size=batch_size,
-                                                         class_mode='categorical')
 
     # fine tune the model
     model.fit_generator(
---
+++ utils.py
@@ -0,0 +1,14 @@
+import multiprocessing
+
+from tensorflow.python.client import device_lib
+
+
+# getting the number of GPUs
+def get_available_gpus():
+    local_device_protos = device_lib.list_local_devices()
+    return [x.name for x in local_device_protos if x.device_type == 'GPU']
+
+
+# getting the number of CPUs
+def get_available_cpus():
+    return multiprocessing.cpu_count()
