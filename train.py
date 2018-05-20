7f638970d1917edf56ced471893da04c5221f1a6
Yang Liu <foamliu@yeah.net>
2018-05-20 15:03:39 +0800

    AttributeError: 'NoneType' object has no attribute 'update'

--- train.py
+++ train.py
@@ -37,14 +37,14 @@ if __name__ == '__main__':
                                                          class_mode='categorical', shuffle=True)
 
 
-    class MyCbk(keras.callbacks.Callback):
-        def __init__(self, model):
-            keras.callbacks.Callback.__init__(self)
-            self.model_to_save = model
-
-        def on_epoch_end(self, epoch, logs=None):
-            fmt = 'models/model.%02d-%.4f.hdf5'
-            self.model_to_save.save(fmt % (epoch, logs['val_acc']))
+    # class MyCbk(keras.callbacks.Callback):
+    #     def __init__(self, model):
+    #         keras.callbacks.Callback.__init__(self)
+    #         self.model_to_save = model
+    #
+    #     def on_epoch_end(self, epoch, logs=None):
+    #         fmt = 'models/model.%02d-%.4f.hdf5'
+    #         self.model_to_save.save(fmt % (epoch, logs['val_acc']))
 
 
     # Callbacks
@@ -55,17 +55,17 @@ if __name__ == '__main__':
     model_names = trained_models_path + '.{epoch:02d}-{val_acc:.4f}.hdf5'
     model_checkpoint = ModelCheckpoint(model_names, monitor='val_acc', verbose=1, save_best_only=True)
 
-    num_gpu = len(get_available_gpus())
-    if num_gpu >= 2:
-        with tf.device("/cpu:0"):
-            model = resnet152_model(img_rows=img_height, img_cols=img_width, color_type=num_channels,
-                                    num_classes=num_classes)
-
-        model = multi_gpu_model(model, gpus=num_gpu)
-        # rewrite the callback: saving through the original model and not the multi-gpu model.
-        model_checkpoint = MyCbk(model)
-    else:
-        model = resnet152_model(img_rows=img_height, img_cols=img_width, color_type=num_channels,
+    # num_gpu = len(get_available_gpus())
+    # if num_gpu >= 2:
+    #     with tf.device("/cpu:0"):
+    #         model = resnet152_model(img_rows=img_height, img_cols=img_width, color_type=num_channels,
+    #                                 num_classes=num_classes)
+    #
+    #     model = multi_gpu_model(model, gpus=num_gpu)
+    #     # rewrite the callback: saving through the original model and not the multi-gpu model.
+    #     model_checkpoint = MyCbk(model)
+    # else:
+    model = resnet152_model(img_rows=img_height, img_cols=img_width, color_type=num_channels,
                                 num_classes=num_classes)
 
     sgd = SGD(lr=1e-3, decay=1e-6, momentum=0.9, nesterov=True)
