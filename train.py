d288a1cca38bb43eeb0d22789f5193cf12b843c2
Yang Liu <foamliu@yeah.net>
2018-05-20 15:16:29 +0800

    update

--- train.py
+++ train.py
@@ -17,7 +17,7 @@ num_classes = 80
 num_train_samples = 53879
 num_valid_samples = 7120
 verbose = 1
-batch_size = 6
+batch_size = 12
 num_epochs = 1000
 patience = 50
 
@@ -37,35 +37,35 @@ if __name__ == '__main__':
                                                          class_mode='categorical', shuffle=True)
 
 
-    # class MyCbk(keras.callbacks.Callback):
-    #     def __init__(self, model):
-    #         keras.callbacks.Callback.__init__(self)
-    #         self.model_to_save = model
-    #
-    #     def on_epoch_end(self, epoch, logs=None):
-    #         fmt = 'models/model.%02d-%.4f.hdf5'
-    #         self.model_to_save.save(fmt % (epoch, logs['val_acc']))
+    class MyCbk(keras.callbacks.Callback):
+        def __init__(self, model):
+            keras.callbacks.Callback.__init__(self)
+            self.model_to_save = model
+
+        def on_epoch_end(self, epoch, logs=None):
+            fmt = 'models/model.%02d-%.4f.hdf5'
+            self.model_to_save.save(fmt % (epoch, logs['val_loss']))
 
 
     # Callbacks
     tensor_board = keras.callbacks.TensorBoard(log_dir='./logs', histogram_freq=0, write_graph=True, write_images=True)
-    early_stop = EarlyStopping('val_acc', patience=patience)
-    reduce_lr = ReduceLROnPlateau('val_acc', factor=0.1, patience=int(patience / 4), verbose=1)
+    early_stop = EarlyStopping('val_loss', patience=patience)
+    reduce_lr = ReduceLROnPlateau('val_loss', factor=0.1, patience=int(patience / 4), verbose=1)
     trained_models_path = 'models/model'
-    model_names = trained_models_path + '.{epoch:02d}-{val_acc:.4f}.hdf5'
+    model_names = trained_models_path + '.{epoch:02d}-{val_loss:.4f}.hdf5'
     model_checkpoint = ModelCheckpoint(model_names, monitor='val_acc', verbose=1, save_best_only=True)
 
-    # num_gpu = len(get_available_gpus())
-    # if num_gpu >= 2:
-    #     with tf.device("/cpu:0"):
-    #         model = resnet152_model(img_rows=img_height, img_cols=img_width, color_type=num_channels,
-    #                                 num_classes=num_classes)
-    #
-    #     model = multi_gpu_model(model, gpus=num_gpu)
-    #     # rewrite the callback: saving through the original model and not the multi-gpu model.
-    #     model_checkpoint = MyCbk(model)
-    # else:
-    model = resnet152_model(img_rows=img_height, img_cols=img_width, color_type=num_channels,
+    num_gpu = len(get_available_gpus())
+    if num_gpu >= 2:
+        with tf.device("/cpu:0"):
+            model = resnet152_model(img_rows=img_height, img_cols=img_width, color_type=num_channels,
+                                    num_classes=num_classes)
+
+        model = multi_gpu_model(model, gpus=num_gpu)
+        # rewrite the callback: saving through the original model and not the multi-gpu model.
+        model_checkpoint = MyCbk(model)
+    else:
+        model = resnet152_model(img_rows=img_height, img_cols=img_width, color_type=num_channels,
                                 num_classes=num_classes)
 
     sgd = SGD(lr=1e-3, decay=1e-6, momentum=0.9, nesterov=True)
