7da9ab1aaee22af414cd7072df2f60849a29dc67
Yang Liu <foamliu@yeah.net>
2018-05-22 07:55:06 +0800

    greedy

--- train.py
+++ train.py
@@ -18,7 +18,7 @@ if __name__ == '__main__':
     num_train_samples = 53879
     num_valid_samples = 7120
     verbose = 1
-    batch_size = 10
+    batch_size = 20
     num_epochs = 10
     patience = 50
 
