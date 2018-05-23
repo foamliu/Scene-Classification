0fed222b4408374f4f1e758fa09c8c8362a64c7d
Yang Liu <foamliu@yeah.net>
2018-05-23 12:55:18 +0800

    152 -> 50

--- demo.py
+++ demo.py
@@ -7,7 +7,7 @@ import cv2 as cv
 import keras.backend as K
 import numpy as np
 
-from resnet_152 import resnet152_model
+from resnet_50 import resnet50_model
 from utils import draw_str
 
 if __name__ == '__main__':
@@ -15,8 +15,8 @@ if __name__ == '__main__':
     num_channels = 3
     num_classes = 80
 
-    model = resnet152_model(img_rows=img_height, img_cols=img_width, color_type=num_channels,
-                            num_classes=num_classes)
+    model = resnet50_model(img_rows=img_height, img_cols=img_width, color_type=num_channels,
+                           num_classes=num_classes)
     model.load_weights('models/model.06-0.7459.hdf5')
 
     with open('scene_classes.csv') as file:
