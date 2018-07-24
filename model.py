529497fd1890f6b4b394adff41bfdb6c892ddbd4
Yang Liu <foamliu@yeah.net>
2018-07-24 13:45:06 +0800

    regularization

--- model.py
+++ model.py
@@ -1,5 +1,5 @@
 from keras.applications.inception_resnet_v2 import InceptionResNetV2
-from keras.layers import Dense, GlobalAveragePooling2D
+from keras.layers import Dense, GlobalAveragePooling2D, Dropout
 from keras.models import Model
 
 from config import num_classes
@@ -9,7 +9,9 @@ def build_model():
     base_model = InceptionResNetV2(weights='imagenet', include_top=False)
     x = base_model.output
     x = GlobalAveragePooling2D()(x)
+	x = Dropout(0.2)(x)
     x = Dense(1024, activation='relu')(x)
+	x = Dropout(0.2)(x)
     predictions = Dense(num_classes, activation='softmax')(x)
     model = Model(inputs=base_model.input, outputs=predictions)
     return model
