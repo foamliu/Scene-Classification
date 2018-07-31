32dd3924234adf29c1b9330cf54b7d9a476e33fb
Yang Liu <foamliu@yeah.net>
2018-07-31 14:39:58 +0800

    update

--- hp_search.py
+++ hp_search.py
@@ -1,12 +1,17 @@
 from __future__ import print_function
 
-import keras.backend as K
-import tensorflow as tf
+import os
+from math import log
+
+import keras
 from hyperas import optim
-from hyperas.distributions import choice, uniform
+from hyperas.distributions import loguniform
+from hyperas.distributions import uniform
 from hyperopt import Trials, STATUS_OK, tpe
+from keras import regularizers
 from keras.applications.inception_resnet_v2 import InceptionResNetV2, preprocess_input
-from keras.layers.core import Dense, Dropout, Activation
+from keras.layers import GlobalAveragePooling2D
+from keras.layers.core import Dense, Dropout
 from keras.models import Model
 from keras.preprocessing.image import ImageDataGenerator
 
@@ -33,22 +38,25 @@ def data():
 
 
 def create_model(train_generator, validation_generator):
-    base_model = InceptionResNetV2(weights='imagenet', pooling='avg', include_top=False)
+    l2_reg = regularizers.l2({{loguniform(log(1e-6), log(1e-2))}})
+    base_model = InceptionResNetV2(weights='imagenet', include_top=False)
     x = base_model.output
+    x = GlobalAveragePooling2D()(x)
     x = Dropout({{uniform(0, 1)}})(x)
-    x = Dense({{choice([512, 1024, 1536])}})(x)
-    x = Activation({{choice(['relu', 'elu', 'linear'])}})(x)
+    x = Dense(1024, activation='relu', kernel_regularizer=l2_reg, activity_regularizer=l2_reg)(x)
     x = Dropout({{uniform(0, 1)}})(x)
-    predictions = Dense(num_classes, activation='softmax')(x)
+    predictions = Dense(num_classes, activation='softmax', kernel_regularizer=l2_reg, activity_regularizer=l2_reg)(x)
     model = Model(inputs=base_model.input, outputs=predictions)
-    graph = tf.get_default_graph()
+
+    model_weights_path = os.path.join('models', best_model)
+    model.load_weights(model_weights_path)
 
     for i in range(int(len(base_model.layers) * {{uniform(0, 1)}})):
         layer = base_model.layers[i]
         layer.trainable = False
 
-    model.compile(loss='categorical_crossentropy', metrics=['accuracy'],
-                  optimizer={{choice(['rmsprop', 'adam'])}})
+    adam = keras.optimizers.Adam(lr={{loguniform(log(1e-6), log(1e-3))}})
+    model.compile(loss='categorical_crossentropy', metrics=['accuracy'], optimizer=adam)
 
     # print(model.summary())
 
@@ -58,11 +66,8 @@ def create_model(train_generator, validation_generator):
         validation_data=validation_generator,
         validation_steps=num_valid_samples // batch_size // 10)
 
-    global graph
-    with graph.as_default():
-        score, acc = model.evaluate_generator(validation_generator)
+    score, acc = model.evaluate_generator(validation_generator)
     print('Test accuracy:', acc)
-    K.clear_session()
     return {'loss': -acc, 'status': STATUS_OK, 'model': model}
 
 
@@ -71,7 +76,7 @@ if __name__ == '__main__':
     best_run, best_model = optim.minimize(model=create_model,
                                           data=data,
                                           algo=tpe.suggest,
-                                          max_evals=20,
+                                          max_evals=15,
                                           trials=Trials())
 
     print("Evalutation of best performing model:")
--- model.py
+++ model.py
@@ -9,9 +9,7 @@ def build_model():
     base_model = InceptionResNetV2(weights='imagenet', include_top=False)
     x = base_model.output
     x = GlobalAveragePooling2D()(x)
-    x = Dropout(0.2)(x)
     x = Dense(1024, activation='relu')(x)
-    x = Dropout(0.2)(x)
     predictions = Dense(num_classes, activation='softmax')(x)
     model = Model(inputs=base_model.input, outputs=predictions)
     return model
