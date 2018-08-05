d9f9e3257562781b1c55acfe92fdca2b83a69304
Yang Liu <foamliu@yeah.net>
2018-08-05 11:32:48 +0800

    update

--- demo.py
+++ demo.py
@@ -1,5 +1,6 @@
 # import the necessary packages
 import csv
+import json
 import os
 import random
 
@@ -10,7 +11,6 @@ from keras.applications.inception_resnet_v2 import preprocess_input
 
 from config import best_model
 from model import build_model
-from utils import draw_str
 
 if __name__ == '__main__':
     model = build_model()
@@ -23,7 +23,7 @@ if __name__ == '__main__':
 
     scene_classes_dict = dict()
     for item in scene_classes_list:
-        scene_classes_dict[int(item[0])] = item[2]
+        scene_classes_dict[int(item[0])] = item[1]
 
     test_path = 'data/test_a/'
     test_images = [f for f in os.listdir(test_path) if
@@ -34,6 +34,7 @@ if __name__ == '__main__':
     if not os.path.exists('images'):
         os.makedirs('images')
 
+    results = []
     for i in range(len(samples)):
         image_name = samples[i]
         filename = os.path.join(test_path, image_name)
@@ -46,8 +47,10 @@ if __name__ == '__main__':
         prob = np.max(preds)
         class_id = np.argmax(preds)
         print(scene_classes_dict[class_id])
+        results.append({'label': scene_classes_dict[class_id], 'prob': prob})
         text = ('{}, prob: {:.4}'.format(scene_classes_dict[class_id], prob))
-        draw_str(image, (5, 15), text)
         cv.imwrite('images/{}_out.png'.format(i), image)
+    with open('results.json', 'w') as file:
+        json.dump(results, file)
 
     K.clear_session()
--- model.py
+++ model.py
@@ -1,23 +1,15 @@
-from keras import regularizers
 from keras.applications.inception_resnet_v2 import InceptionResNetV2
-from keras.layers import Dense, GlobalAveragePooling2D, Dropout
+from keras.layers import Dense, GlobalAveragePooling2D
 from keras.models import Model
 
 from config import num_classes
 
 
 def build_model():
-    l2_reg = regularizers.l2(2e-6)
     base_model = InceptionResNetV2(weights='imagenet', include_top=False)
     x = base_model.output
-    for i in range(int(len(base_model.layers) * 0.73)):
-        layer = base_model.layers[i]
-        layer.trainable = False
-
     x = GlobalAveragePooling2D()(x)
-    x = Dropout(0.29)(x)
-    x = Dense(1024, activation='relu', kernel_regularizer=l2_reg, activity_regularizer=l2_reg)(x)
-    x = Dropout(0.25)(x)
-    predictions = Dense(num_classes, activation='softmax', kernel_regularizer=l2_reg, activity_regularizer=l2_reg)(x)
+    x = Dense(1024, activation='relu')(x)
+    predictions = Dense(num_classes, activation='softmax')(x)
     model = Model(inputs=base_model.input, outputs=predictions)
     return model
